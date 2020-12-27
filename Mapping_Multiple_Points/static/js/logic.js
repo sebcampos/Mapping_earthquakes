f37b3cf4f752b8ac8b3198cf179af661f5ba49c6
sebcampos <sebcampos@criptext.com>
2020-12-26 22:16:04 -0800

    finished 13.4.2

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -8,13 +8,31 @@ let map = L.map('mapid', {
 });
 
 
+
+// `Import` cities object into our script as cityData
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000,
+        color: "#ffa500",
+        fillColor: "#ffa500",
+        fillOpacity: 0.2 //<- default setting
+ }).bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> </hr> <h3>Population " + city.population.toLocaleString() + "<h3>").addTo(map);
+
+   });
+
+
+
 //  Add a marker to the map for Los Angeles, California.
-L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color: "black",
-    fillColor: "#FFFF00",
-    fillOpacity: 0.2 //<- default setting
- }).addTo(map);
+// L.circleMarker([34.0522, -118.2437], {
+//     radius: 300,
+//     color: "black",
+//     fillColor: "#FFFF00",
+//     fillOpacity: 0.2 //<- default setting
+//  }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
